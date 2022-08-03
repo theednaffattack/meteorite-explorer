470a75c1bc50d6896dbc8c6d6e9b0f70fc6fa731
Eddie Naff <eddie@eddienaff.dev>
2022-08-03 10:03:06 -0700

    Add error handling to request

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,13 +5,15 @@ import path from "path";
 import debug from "debug";
 import express from "express";
 import { createClient } from "redis";
-import { internalIpV4 } from "./internal-ip";
 
 // Local imports below
 import { asyncWrap } from "./async-wrap";
+import { internalIpV4 } from "./internal-ip";
 import { generalErrorHandler } from "./general-error-handler";
 import { getRedisData } from "./get-redis-data";
 import { setRedisData } from "./set-redis-data";
+import { clamp } from "./clamp";
+import type { MeteorApiType } from "./types";
 
 type ConfigVars = {
 	meteoriteUrl: string;
@@ -19,6 +21,45 @@ type ConfigVars = {
 	redisUrl: string;
 	redisOpts: { url: string };
 };
+
+process.on("error", (err) => {
+	console.log("PROCESS ON", { err });
+});
+
+const redisUsername = "";
+const redisHostname = "192.168.1.16";
+// const redisHostname = "localhost";
+const redisPort = 16379;
+
+const redisUrl = `redis://${redisHostname}:${redisPort}?ConnectTimeout=5000&IdleTimeOutSecs=180`;
+// redis://localhost:6379?ConnectTimeout=5000&IdleTimeOutSecs=180
+
+export const redisClient = createClient({
+	// socket: { host: redisHostname, port: redisPort },
+	url: redisUrl,
+});
+
+redisClient
+	.on("connect", () => {
+		console.log(`Connected to redis`);
+		debug("Redis connect");
+	})
+	.on("ready", () => {
+		debug("Redis ready");
+	})
+	.on("error", (e: unknown) => {
+		console.log(`Redis error ${e}`);
+		debug(`Redis error ${e}`);
+	})
+	.on("close", () => {
+		debug("Redis close");
+	})
+	.on("reconnecting", () => {
+		debug("Redis reconnecting");
+	})
+	.on("end", () => {
+		debug("Redis end");
+	});
 
 async function main() {
 	// Create a key mapper to grab env variables.
@@ -37,28 +78,20 @@ async function main() {
 	if (configData === null) throw new Error("Config data is null!");
 	if (configErr) generalErrorHandler(configErr);
 
-	const redisClient = createClient({ url: configData.redisUrl });
+	try {
+		await redisClient.connect();
+		console.log("Awaiting Redis Connection");
+	} catch (redisErr) {
+		console.error("UNKNOWN REDIS ERROR", redisErr);
+	}
 
 	// Add redis events in case we want to log this later
-	redisClient
-		.on("connect", () => {
-			debug("Redis connect");
-		})
-		.on("ready", () => {
-			debug("Redis ready");
-		})
-		.on("error", (e: unknown) => {
-			debug(`Redis error ${e}`);
-		})
-		.on("close", () => {
-			debug("Redis close");
-		})
-		.on("reconnecting", () => {
-			debug("Redis reconnecting");
-		})
-		.on("end", () => {
-			debug("Redis end");
-		});
+
+	// Close the connection when there is an interrupt sent from keyboard
+	process.on("SIGINT", () => {
+		redisClient.quit();
+		console.log("redis client quit");
+	});
 
 	const server = express();
 
@@ -71,20 +104,41 @@ async function main() {
 		res.sendFile(path.join(__dirname, "/index.html"));
 	});
 
-	server.get(
-		"/api/jsonData",
-		(req, res, next) => getRedisData(req, res, next, redisClient),
-		async (req, res) => {
-			const { data: nasaData } = await axios.get(
-				`${configData.meteoriteUrl}?offset=${req.query.page}&$limit=20`,
-			);
-			// ParsedQs can be string or string[] or undefined
-			// so test if the variable is of type "string"
-			if (nasaData && typeof req.query.page === "string") {
-				setRedisData(req.query.page, nasaData, redisClient);
-			}
-		},
-	);
+	server.get("/api/jsonData", getRedisData, async (req, res) => {
+		// Coerce string | string[] | QueryString.ParsedQs |
+		// QueryString.ParsedQs[] to string instead
+		if (typeof req.query.$offset !== "string") return;
+
+		// Clamp the limit value;
+		// Setting num to default 20 is asinine
+		// but the max limit will probably be set
+		// by server config in the future, so I'll leave it
+		const realLimit = clamp({
+			num: Number(req.query.$limit) || 20,
+			min: 0,
+			max: 20,
+		});
+
+		const apiUri = `${configData.meteoriteUrl}?$offset=${req.query.$offset}&$limit=${realLimit}`;
+
+		const [nasaData, nasaDataError] = await asyncWrap(() =>
+			axios.get<MeteorApiType>(apiUri),
+		);
+
+		// Catch any errors here and send them to the client
+		if (nasaDataError) {
+			console.error(nasaDataError);
+			res.status(400).send(nasaDataError);
+		}
+
+		// Set new values in Redis
+		// Respond to the client
+		if (nasaData) {
+			setRedisData(req.query.$offset, nasaData.data, redisClient);
+
+			res.status(200).send(nasaData.data);
+		}
+	});
 
 	server.listen(configData.port, async () => {
 		const ip = await internalIpV4();
