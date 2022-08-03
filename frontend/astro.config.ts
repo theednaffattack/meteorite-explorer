b5ad9cda446d12a65f1756db6adab2931ae803f0
Eddie Naff <eddie@eddienaff.dev>
2022-08-03 10:03:42 -0700

    Adjust configs

--- frontend/astro.config.ts
+++ frontend/astro.config.ts
@@ -5,4 +5,9 @@ import react from "@astrojs/react";
 export default defineConfig({
 	// Enable React to support React JSX components.
 	integrations: [react()],
+	vite: {
+		ssr: {
+			external: ["svgo"],
+		},
+	},
 });
